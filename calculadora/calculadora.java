d585c37df0a471f10020c07bab726207b13d3067
ANDRESFELIPE109 <>
2021-04-19 15:12:17 +0000

    funciona igual

--- calculadora/calculadora.java
+++ calculadora/calculadora.java
@@ -68,17 +68,5 @@ public class calculadora{
     return divi;
   }
 
-  public int getNum1(){
-    return num1;
-  }
-  public void setNum1(int num1){
-    this.num1 = num1;
-  }
-  public int getNum2(){
-    return num2;
-  }
-  public void setNum2(int num1){
-    this.num2 = num2;
-  }
 
 }

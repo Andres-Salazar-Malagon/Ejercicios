94d0e2540748708f0a49646904ca7fed6d5c1b4d
ANDRESFELIPE109 <>
2021-04-20 15:26:33 +0000

    limite 2

--- limite/limite.java
+++ limite/limite.java
@@ -4,16 +4,29 @@ public class limite{
   static int num1;
   static int num2;
   public static  void main(String[] args){
-    System.out.print("digite el priemer nuemero: ");
+    int i;
+
+    System.out.print("digite el limite inferior: ");
     Scanner entrada = new Scanner(System.in);
     num1 = entrada.nextInt();
 
-    System.out.print("digite el segundo nuemero: ");
+    System.out.print("digite el limite superior: ");
     Scanner entrada2 = new Scanner(System.in);
     num2 = entrada2.nextInt();
 
     System.out.println(num1);
     System.out.println(num2);
+
+
+
+    for(i=num1; i<=num2; i++){
+      System.out.println(i);
+      if(i % 2 == 0){
+        System.out.println("es numero par: "+num1);
+      }else{
+        System.out.println("no es par: "+num1);
+      }
+    }
   }
 
 

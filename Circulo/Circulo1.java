15f51536c4c53c8e3f4b66a88ae1462efdd12db0
ANDRESFELIPE109 <>
2021-04-21 17:18:22 +0000

    1

--- Circulo/Circulo1.java
+++ Circulo/Circulo1.java
@@ -2,21 +2,22 @@ import java.awt.*;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 
-public class Saludo extends JApplet {
+public class Circulo1 extends JApplet {
   
   public void paint (Graphics g) {
     int x;
     int y;
-    for(x=50; x<=100)
-      g.drawString ("Hola", 50, 50);
+    for(x=50; x<=100; x++){
+      g.drawString (".", x, 50);
+    }
     
   }
 
   public static void main (String[] args){
     JFrame frame = new JFrame ("Cargando JApplet");
-    Saludo saludoApplet = new Saludo();
-    saludoApplet.init();
-    frame.getContentPane().add(saludoApplet);
+    Circulo1 circulo1Applet = new Circulo1();
+    circulo1Applet.init();
+    frame.getContentPane().add(circulo1Applet);
     frame.setSize(200,200);
     frame.setVisible(true);
   }
--- Circulo/Circulo2.java
+++ Circulo/Circulo2.java
@@ -2,21 +2,22 @@ import java.awt.*;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 
-public class Saludo extends JApplet {
+public class Circulo2 extends JApplet {
   
   public void paint (Graphics g) {
-    int x;
-    int y;
-    for(x=50; x<=100)
-      g.drawString ("Hola", 50, 50);
+    int i;
+    int j;
+    for(i=50; i<=i-1; i++){
+      g.drawString (".", i, i);
+    };
     
   }
 
   public static void main (String[] args){
     JFrame frame = new JFrame ("Cargando JApplet");
-    Saludo saludoApplet = new Saludo();
-    saludoApplet.init();
-    frame.getContentPane().add(saludoApplet);
+    Circulo2 circulo2Applet = new Circulo2();
+    circulo2Applet.init();
+    frame.getContentPane().add(circulo2Applet);
     frame.setSize(200,200);
     frame.setVisible(true);
   }

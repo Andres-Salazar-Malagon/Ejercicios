240a5dd611adf265420c33cbff2533194a7db139
ANDRESFELIPE109 <>
2021-04-21 17:11:29 +0000

    un intento

---
+++ Circulo/Circulo2.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+
+public class Saludo extends JApplet {
+  
+  public void paint (Graphics g) {
+    int x;
+    int y;
+    for(x=50; x<=100)
+      g.drawString ("Hola", 50, 50);
+    
+  }
+
+  public static void main (String[] args){
+    JFrame frame = new JFrame ("Cargando JApplet");
+    Saludo saludoApplet = new Saludo();
+    saludoApplet.init();
+    frame.getContentPane().add(saludoApplet);
+    frame.setSize(200,200);
+    frame.setVisible(true);
+  }
+}

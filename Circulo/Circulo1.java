4ce19dd73916d0666e7f54cd059254e4a9e456dc
ANDRESFELIPE109 <>
2021-04-22 16:35:59 +0000

    porfin funciona

--- Circulo/Circulo1.java
+++ Circulo/Circulo1.java
@@ -3,14 +3,453 @@ import javax.swing.JApplet;
 import javax.swing.JFrame;
 
 public class Circulo1 extends JApplet {
-  
+    int x=100;
+    int y=100;
+    int z=1;
   public void paint (Graphics g) {
-    int x;
-    int y;
-    for(x=50; x<=100; x++){
-      g.drawString (".", x, 50);
+
+    while(z<50){
+      g.drawString (".", x, y);
+      x+=1;
+      z+=1;
     }
+    x=100;
+    z=1;
+
+    while(z<=50){
+      g.drawString (".", x, y);
+      y+=1;
+      z+=1;
+    }
+    y=100;
+    z=1;
+
+    while(z<=50){
+      g.drawString (".", x, y);
+      x-=1;
+      z+=1;
+    }
+    x=100;
+    z=1;
+
+    while(z<=50){
+      g.drawString (".", x, y);
+      y-=1;
+      z+=1;
+    }
+    y=100;
+    z=1;
+
+    while(z<=51){
+      g.drawString (".", x, y);
+      x+=10;
+      y+=1;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=51){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=10;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+  
+    while(z<=51){
+      g.drawString (".", x, y);
+      x-=10;
+      y-=1;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=51){
+      g.drawString (".", x, y);
+      x+=1;
+      y-=10;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=51){
+      g.drawString (".", x, y);
+      x-=10;
+      y+=1;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=51){
+      g.drawString (".", x, y);
+      x-=1;
+      y-=10;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+  
+    while(z<=51){
+      g.drawString (".", x, y);
+      x+=10;
+      y-=1;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=51){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=10;
+      z+=10;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=37){
+      g.drawString (".", x, y);
+      x+=1;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=37){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=37){
+      g.drawString (".", x, y);
+      x-=1;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=37){
+      g.drawString (".", x, y);
+      x+=1;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x+=2;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x+=3;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x+=5;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x+=3;
+      y+=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
     
+    while(z<=24){
+      g.drawString (".", x, y);
+      x+=1;
+      y+=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x+=1;
+      y+=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x+=1;
+      y+=5;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x+=2;
+      y+=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x-=1;
+      y+=5;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x-=2;
+      y+=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x-=2;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x-=3;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x-=5;
+      y+=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x-=3;
+      y+=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    
+    while(z<=24){
+      g.drawString (".", x, y);
+      x-=2;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x-=3;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x-=5;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x-=3;
+      y-=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x-=1;
+      y-=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x-=1;
+      y-=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x-=1;
+      y-=5;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x-=2;
+      y-=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x+=1;
+      y-=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x+=1;
+      y-=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x+=1;
+      y-=5;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x+=2;
+      y-=3;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+
+    while(z<=24){
+      g.drawString (".", x, y);
+      x+=2;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=17){
+      g.drawString (".", x, y);
+      x+=3;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=11){
+      g.drawString (".", x, y);
+      x+=5;
+      y-=1;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;
+    while(z<=15){
+      g.drawString (".", x, y);
+      x+=3;
+      y-=2;
+      z+=1;
+    }
+    x=100;
+    y=100;
+    z=1;    
+    g.drawOval(50,48,102,102);
+   
   }
 
   public static void main (String[] args){
